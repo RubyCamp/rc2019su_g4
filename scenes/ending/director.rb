69a161b02595e6b0212e4fc7fa643c8214218d98
No-jun <pelm5zmd@s.okayama-u.ac.jp>
2019-08-22 13:28:56 +0900

    endroll

--- scenes/ending/director.rb
+++ scenes/ending/director.rb
@@ -1,15 +1,53 @@
-module Ending
+﻿module Ending
   class Director
+    FONT_SIZE = 50
     def initialize
-      @bg_img = Image.load("images/background_ending.png")
-      @timer = 3 * 60
+      Window.bgcolor = [255, 255, 255, 255]
+      @image = Image.load('images/imageB.png')
+      @image_scroll_y = 0
+      @timer = 30 * 60
+      @words = %w(
+「製作者」
+	AmiYamamoto
+	石橋惇
+	佐々木コウシ
+	Nojun
+	ロリコン『あっきー＠妹募集中』
+	.
+	.
+		.
+	「協力」
+	Ruby合宿
+	島根県
+	青少年自然の家サン・レイク
+	.
+	.
+	.
+	.
+	ThankYou!!
+        )
+      @font = Font.new(FONT_SIZE, 'MS 明朝', weight: true, color: [0,0,0])
+      @scroll_y = 0
     end
 
     def play
-      Window.draw(0, 0, @bg_img)
+      @image_scroll_y += 1
+      image_draw_x = 0
+      image_draw_y = Window.height - @image_scroll_y	
+      Window.draw(image_draw_x, image_draw_y, @image)
+       
+      @scroll_y += 1
+      frame_out = []
       @timer -= 1
-      if @timer <= 0
-        exit
+      
+	 @words.each_with_index do |word, i|
+          draw_x = Window.width / 3
+          draw_y = (Window.height + i * FONT_SIZE + 600) - @scroll_y
+	  color = [0,0,0]
+          Window.draw_font(draw_x, draw_y, word, @font, color: color)
+	if @timer <= 0
+	   exit
+	 end
       end
     end
   end
